9ba5e172c2f936d7902370c2843476dff0d630b5
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-05 19:53:38 -0300

    Initial commit.

---
+++ ios_calculator/ios_calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios_calculator
+//
+//  Created by Paula Hasstenteufel Denz on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
