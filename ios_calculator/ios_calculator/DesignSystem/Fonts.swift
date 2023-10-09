3cb4309efdb88e953c3aeb910a9be19070a55cb8
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-09 08:53:59 -0300

    Adding a Roboto font pack to later implement Typography DesignSystem basic types.

---
+++ ios_calculator/ios_calculator/DesignSystem/Fonts.swift
@@ -0,0 +1,19 @@
+//
+//  Fonts.swift
+//  ios_calculator
+//
+//  Created by Paula Hasstenteufel Denz on 05/10/23.
+//
+
+import SwiftUI
+
+enum Fonts {
+    static let heading: Font
+    static let title: Font
+    static let detail: Font
+    static let caption: Font
+    static let sidebar: Font
+    static let body: Font
+    static let keyboard: Font
+    static let display: Font
+}
