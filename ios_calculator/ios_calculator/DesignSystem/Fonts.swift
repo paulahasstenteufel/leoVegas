5658cc7ade89d7fbcfe26314a35de6d22e3f467d
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-09 10:45:38 -0300

    Adding small helper to convert fonts and creating basics sizes, which will be refined later on.

--- ios_calculator/ios_calculator/DesignSystem/Fonts.swift
+++ ios_calculator/ios_calculator/DesignSystem/Fonts.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 enum Fonts {
-    static let heading: Font
-    static let title: Font
-    static let detail: Font
-    static let caption: Font
-    static let sidebar: Font
-    static let body: Font
-    static let keyboard: Font
-    static let display: Font
+    static let heading: Font =  .roboto(.medium, size: 22)
+    static let title: Font = .roboto(.medium, size: 18)
+    static let detail: Font = .roboto(.regular, size: 16)
+    static let caption: Font = .roboto(.medium, size: 16)
+    static let sidebar: Font = .roboto(.regular, size: 14)
+    static let body: Font = .roboto(.regular, size: 20)
+    static let keyboard: Font = .roboto(.regular, size: 50)
+    static let display: Font = .roboto(.regular, size: 100)
+}
+
+extension Font {
+    static func roboto(_ type: Roboto, size: CGFloat) -> Self {
+        let font = UIFont(name: type.rawValue, size: size) ?? UIFont.systemFont(ofSize: size)
+        return .init(uiFont: font)
+    }
+    
+    private init(uiFont: UIFont) {
+        self = Font(uiFont as CTFont)
+    }
+}
+
+enum Roboto: String {
+    case black = "Roboto-Black"
+    case bold = "Roboto-Bold"
+    case italic = "Roboto-Italic"
+    case light = "Roboto-Light"
+    case medium = "Roboto-Medium"
+    case regular = "Roboto-Regular"
+    case thin = "Roboto-Thin"
 }
