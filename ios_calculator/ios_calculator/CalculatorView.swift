de4d1e42376821e5313facf478789d4c64e6a5d1
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-09 09:04:23 -0300

    Basic view for number input visual feedback.

--- ios_calculator/ios_calculator/CalculatorView.swift
+++ ios_calculator/ios_calculator/CalculatorView.swift
@@ -18,4 +18,23 @@ struct CalculatorView: View {
     //MARK: Private
     @StateObject
     private var viewModel = CalculatorViewModel()
+    
+    private var inputView: some View {
+        HStack {
+            Text(viewModel.rawInput)
+                .padding(.leading)
+            
+            Spacer()
+            
+            Image(systemName: "xmark")
+                .padding(.trailing)
+                .onTapGesture {
+                    viewModel.clear()
+                }
+        }
+        .font(Littera.heading)
+        .padding([.top, .bottom], 30)
+        .border(Chroma.Neutral.medium, width: 1)
+        .cornerRadius(DrawingConstants.cornerRadius)
+    }
 }
