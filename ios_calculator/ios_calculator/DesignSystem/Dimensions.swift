942512110a727b6b68e251380e9ae17138377048
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-09 08:55:14 -0300

    A file to keep track of all reusable sizes, padding and spacings in the app.

---
+++ ios_calculator/ios_calculator/DesignSystem/Dimensions.swift
@@ -0,0 +1,22 @@
+//
+//  Dimensions.swift
+//  ios_calculator
+//
+//  Created by Paula Hasstenteufel Denz on 08/10/23.
+//
+
+import SwiftUI
+
+enum Dimension: CGFloat {
+    case primary = 18
+    case small = 10
+//    case specialKeyWidth = 250
+}
+
+struct KeyDrawingConstants {
+    static let cornerRadius: CGFloat = 4
+    static let rightButtonsAspectRatio: CGFloat = 1.8
+    static let lineWidth: CGFloat = 2
+    static let numbersAspectRatio: CGFloat = 1.2
+//    static let keyboardFrame: CGSize = .init(width: 350, height: 500)
+}
