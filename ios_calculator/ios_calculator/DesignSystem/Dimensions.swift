e4002176704033a948ce46fedac2f23b1fda07d7
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-10 14:49:45 -0300

    Small improvements to Dimensions for easier reading and writing.

--- ios_calculator/ios_calculator/DesignSystem/Dimensions.swift
+++ ios_calculator/ios_calculator/DesignSystem/Dimensions.swift
@@ -11,12 +11,10 @@ enum Dimension: CGFloat {
     case primary = 18
     case small = 10
 //    case specialKeyWidth = 250
-}
-
-struct KeyDrawingConstants {
-    static let cornerRadius: CGFloat = 4
-    static let rightButtonsAspectRatio: CGFloat = 1.8
-    static let lineWidth: CGFloat = 2
-    static let numbersAspectRatio: CGFloat = 1.2
 //    static let keyboardFrame: CGSize = .init(width: 350, height: 500)
+    
+    static let keyCorner = 4.0
+    static let aspectSquare = 1.2
+    static let aspectRect = 2.48
+    static let line = 2.0
 }
