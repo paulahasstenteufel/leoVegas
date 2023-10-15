4fccf78b183941b482c24bd0e04841bf7f14f6be
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-15 20:03:16 -0300

    Working on ToggleManager logic, also adding a Model and ViewModel for each toggle control.

--- ios_calculator/ios_calculator/FeatureManagers/ToggleManager.swift
+++ ios_calculator/ios_calculator/FeatureManagers/ToggleManager.swift
@@ -5,47 +5,98 @@
 //  Created by Paula Hasstenteufel Denz on 09/10/23.
 //
 
-import Foundation
+import SwiftUI
 
 class ToggleManager: ObservableObject {
     
     @Published
-    var addition = Control(.add)
+    var fundamentals: [Operation]
     
-    @Published
-    var subtraction = Control(.subtract)
-    
-    @Published
-    var division = Control(.divide)
-    
-    @Published
-    var multiplication = Control(.multiply)
-    
-    @Published
-    var sin = Control(.sin)
-    
-    @Published
-    var cos = Control(.cos)
-}
-
-extension ToggleManager {
-    class Control {
-        
-        @Published
-        var isEnabled: Bool
-        
-        init(_ operation: Operation) {
-            self.operation = operation
-            self.isEnabled = true
-        }
-        
-        func toggleFeature() {
-            isEnabled.toggle()
-            //TODO: Probably won't need method, just listening to the publisher
-        }
-        
-        //MARK: Private
-        private let operation: Operation
+    init() {
+        fundamentals = Operation.allFundamental
     }
 }
 
+class ControlViewModel: ObservableObject {
+    
+    @Published
+    var enabled: Bool = true
+    
+    @ObservedObject
+    var toggleManager: ToggleManager
+    
+    let control: Control
+    
+    init(_ model: Control, manager: ToggleManager) {
+        self.control = model
+        self.toggleManager = manager
+    }
+    
+    func toggleFeature() {
+        enabled.toggle()
+        
+        if enabled {
+            insert()
+            
+        } else {
+            remove()
+        }
+    }
+    
+    //MARK: Private
+    private func insert() {
+        guard let position = control.position else {
+            return
+        }
+        
+        toggleManager.fundamentals.insert(control.operation, at: position)
+    }
+    
+    private func remove() {
+        guard let position = control.position else {
+            return
+        }
+        
+        toggleManager.fundamentals.remove(at: position)
+    }
+}
+
+struct Control {
+    let operation: Operation
+    
+    var title: String? {
+        switch operation {
+        case .add: return "Toggle Addition"
+        case .subtract: return "Toggle Subtraction"
+        case .multiply: return "Toggle Multiplication"
+        case .divide: return "Toggle Division"
+//        case .sin: return "Toggle Sin"
+//        case .cos: return "Toggle Cosin"
+        default: return nil
+        }
+    }
+    
+    var image: String? {
+        switch operation {
+        case .add: return "plus.circle.fill"
+        case .subtract: return "minus.circle.fill"
+        case .multiply: return "multiply.circle.fill"
+        case .divide: return "divide.circle.fill"
+//        case .sin: return "waveform.path.ecg"
+//        case .cos: return "waveform.path"
+        default: return nil
+        }
+    }
+        
+    var position: Int? {
+        switch operation {
+        case .add: return 0
+        case .subtract: return 1
+        case .multiply: return 2
+        case .divide: return 3
+//        case .sin: return "waveform.path.ecg"
+//        case .cos: return "waveform.path"
+        default: return nil
+        }
+    }
+}
