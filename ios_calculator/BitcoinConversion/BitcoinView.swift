99a2ea9235dd84ee8a78ff208fc3fc92a468e306
Paula Hasstenteufel Denz <paula@hasstenteufel.com>
2023-10-17 21:33:52 -0300

    Adding Bitcoin button.

--- ios_calculator/BitcoinConversion/BitcoinView.swift
+++ ios_calculator/BitcoinConversion/BitcoinView.swift
@@ -10,35 +10,92 @@ import SwiftUI
 struct BitcoinView: View {
     
     var body: some View {
-        Button {
-            Task.init { await update() }
-            
-        } label: {
+        Button(action: setResult) {
             iconView
+        }
+        .fixedSize()
+        .onAppear {
+            runTask()
         }
     }
     
     //MARK: Private
-    @StateObject
-    private var viewModel = BitcoinViewModel()
+    @EnvironmentObject
+    private var viewModel: CalculatorViewModel
+    
+    @EnvironmentObject
+    private var bitcoinViewModel: BitcoinViewModel
+    
+    @State
+    private var priceFetchingTask: Task<(), Never> = Task { }
     
     private var iconView: some View {
-        let colors = KeyColorSet(
-            foreground: Theme.Neutral.strongest,
-            background: Theme.Neutral.soft,
-            text: Theme.Neutral.strongest
-        )
-        
+        let colors = bitcoinViewModel.rateAvailable ? enabled : disabled
         return keyView(colors, text: "₿")
     }
     
-    private func update() async {
-        do {
-            try await viewModel.getUSDConversionRate()
-            
-        } catch {
-            print(error)
-            //TODO: Add error handling
+    
+    private func setResult() {
+        if let price = bitcoinViewModel.latest, price.updated.inLessThan(300) {
+            viewModel.update(with: price.rate)
+            return
         }
+        
+        runTask()
+    }
+    
+    private func fetchPrice() -> Task<(), Never> {
+        Task {
+            do { try await bitcoinViewModel.getUSDConversionRate() }
+            
+            catch {
+                print(error)
+                //TODO: Handle errors
+            }
+        }
+    }
+    
+    private func runTask() {
+        priceFetchingTask.cancel()
+        priceFetchingTask = fetchPrice()
+    }
+}
+
+extension CalculatorViewModel {
+    func update(with rate: Double) {
+        guard let number = rawInput.number else {
+            //TODO: Handle zeroed input
+            return
+        }
+        
+        let result = number.bitcoinToUSD(rate: rate)
+        display = "USD \(result.display)"
+        
+        clearStacks()
+        clearNext = true
+    }
+}
+
+extension Double {
+    func bitcoinToUSD(rate: Double) -> Double {
+        self * rate
+    }
+}
+
+private extension BitcoinView {
+    var enabled: KeyColorSet {
+        KeyColorSet(
+            foreground: Theme.Feedback.success,
+            background: Theme.Neutral.soft,
+            text: Theme.Feedback.success
+        )
+    }
+    
+    var disabled: KeyColorSet {
+        KeyColorSet(
+            foreground: Theme.Neutral.strong,
+            background: Theme.Neutral.soft,
+            text: Theme.Neutral.strong
+        )
     }
 }
